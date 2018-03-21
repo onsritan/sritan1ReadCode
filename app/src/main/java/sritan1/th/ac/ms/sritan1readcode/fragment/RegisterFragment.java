077df1625910afe1f22213efc7f0c00ffd9309e2
masterpol <chatupol2768@gmail.com>
2018-03-21 12:09:23 +0700

    Create RegisterFragment success

---
+++ app/src/main/java/sritan1/th/ac/ms/sritan1readcode/fragment/RegisterFragment.java
@@ -0,0 +1,25 @@
+package sritan1.th.ac.ms.sritan1readcode.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import sritan1.th.ac.ms.sritan1readcode.R;
+
+/**
+ * Created by asus on 3/21/2018.
+ */
+
+public class RegisterFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        return view;
+    }
+} //Main Class
+
