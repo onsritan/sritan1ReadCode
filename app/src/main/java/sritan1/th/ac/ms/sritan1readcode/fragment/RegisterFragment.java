b849a7798ec69dab8aa1400db2015c340bda9b1b
masterpol <chatupol2768@gmail.com>
2018-03-21 14:48:58 +0700

    Create Toolbar success

--- app/src/main/java/sritan1/th/ac/ms/sritan1readcode/fragment/RegisterFragment.java
+++ app/src/main/java/sritan1/th/ac/ms/sritan1readcode/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package sritan1.th.ac.ms.sritan1readcode.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import sritan1.th.ac.ms.sritan1readcode.MainActivity;
 import sritan1.th.ac.ms.sritan1readcode.R;
 
 /**
@@ -14,6 +16,33 @@ import sritan1.th.ac.ms.sritan1readcode.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+
+//        Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toobarRegister);
+
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title
+             ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+
+//  Show Navigator Icon
+        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        getActivity().getSupportFragmentManager().popBackStack();
+    }
+});
+
+    } // Main Method
+
 
     @Nullable
     @Override
